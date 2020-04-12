2a76d1f7ceba3a8785a7128b6695d9ae2cbab404
a7462593k <55180935+a7462593k@users.noreply.github.com>
2020-04-12 14:29:37 +0800

    4/12

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public static bool complete;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "感應區")
+        {
+            complete = true;
+        }
+    }
+}
